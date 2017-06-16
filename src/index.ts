8a969230bc84aae9903151baece3e2bc7f0ec81c
jsergent <julien.a.sergent@capgemini.com>
2017-06-16 16:29:05 +0200

    feat(input-counter): custom native form component

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { InputCounterComponent } from './input-counter.component';
 
 export * from './input-counter.component';
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   declarations: [
     InputCounterComponent
--- src/input-counter.component.ts
+++ src/input-counter.component.ts
@@ -1,4 +1,13 @@
-import { Component, Input, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, Input, AfterViewInit, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+
+export const CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR: any = {
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => InputCounterComponent),
+    multi: true
+};
+
+const noop = () => {};
 
 @Component({
   selector: 'input-counter',
@@ -9,43 +18,43 @@ import { Component, Input, ViewChild, AfterViewInit } from '@angular/core';
       }
 
       .input-counter-group span {
-        color: #999;
-        text-transform: uppercase;
-        font-size: .5em;
+        color: #b0b0b0;
+        font-size: 10px;
       }
 
-      .input-counter-group text-input-counter {
+      .input-counter-group .text-input-counter {
         position: absolute;
-        right: 10px;
-        bottom -8px;
+        line-height: 10px;
+        right: 0;
+        bottom: -13px;
       }
     </style>
-    <div class="d-inline-block form-group input-counter-group">
-      <input #input-counter type="text" class="form-control" 
-        [(ngModel)]="ngModel" 
-        (keyup)="update()" 
-        (focus)="enableCounter()" 
-        (blur)="disableCounter()">
-      <span *ngIf="enabled" class="text-input-counter">{{ counter }}<span *ngIf="maxlength">/{{ maxlength }}</span></span>
+    <div class="form-group input-counter-group">
+      <input
+        [ngClass]="class" 
+        [maxlength]="maxlength"
+        [(ngModel)]="value"
+        (focus)="onFocus()" 
+        (blur)="onBlur()">
+      <span *ngIf="enabled" class="text-input-counter">{{ counter }}<span *ngIf="maxlength && counter > 0">/{{ maxlength }}</span></span>
     </div>
   `,
+  providers: [CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR]
 })
-export class InputCounterComponent implements AfterViewInit {
-    @Input()
-    ngModel: any;
+export class InputCounterComponent implements AfterViewInit, ControlValueAccessor {
+    private innerValue: any = '';
+    private onTouchedCallback: () => void = noop;
+    private onChangeCallback: (_: any) => void = noop;
+    private counter: number;
+    private enabled: boolean;
 
     @Input()
     maxlength: number;
-
-    @ViewChild('input-counter')
-    private inputCounter: any;
-
-    private counter: number;
-
-    private enabled: boolean;
+    @Input()
+    class = 'form-control';
 
     ngAfterViewInit() {
-      this.update();
+      this.updateCounter();
     }
 
     enableCounter() {
@@ -56,7 +65,47 @@ export class InputCounterComponent implements AfterViewInit {
       this.enabled = false;
     }
 
-    update() {
-      this.counter = this.inputCounter.nativeElement.value.length;
+    onFocus() {
+      this.enableCounter();
+    }
+
+    onBlur() {
+      this.disableCounter();
+      this.onTouchedCallback();
+    }
+
+    updateCounter() {
+      if (this.innerValue) {
+        this.counter = this.innerValue.length;
+      } else {
+        this.counter = 0;
+      }
+    }
+
+    writeValue(value: any) {
+      if (value !== this.innerValue) {
+          this.innerValue = value;
+          this.updateCounter();
+      }
+    }
+
+    registerOnChange(fn: any) {
+        this.onChangeCallback = fn;
+    }
+
+    registerOnTouched(fn: any) {
+        this.onTouchedCallback = fn;
+    }
+
+    get value() {
+      return this.innerValue;
+    }
+
+    set value(value: any) {
+        if (value !== this.innerValue) {
+            this.innerValue = value;
+            this.updateCounter();
+            this.onChangeCallback(value);
+        }
     }
 }
